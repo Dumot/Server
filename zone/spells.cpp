447e15690982c2bf749d093cb468fdef4fca1888
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2025-08-01 03:01:34 -0700

    Prevent Alliance (Faction) spells from triggering /pet assist (#277)

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -4856,7 +4856,7 @@ bool Mob::SpellOnTarget(
 		}
 	}
 
-	if (IsClient() && IsDetrimentalSpell(spell_id) && !IsMesmerizeSpell(spell_id) && !IsHarmonySpell(spell_id) && !IsCharmSpell(spell_id)) {
+	if (IsClient() && IsDetrimentalSpell(spell_id) && !IsMesmerizeSpell(spell_id) && !IsHarmonySpell(spell_id) && !IsCharmSpell(spell_id) && !IsAllianceSpell(spell_id)) {
 		for (auto pet : GetAllPets()) {
 			if (pet && pet->IsNPC() && pet->IsPetAssisting()) {
 				pet->CastToNPC()->DoPetCommandAssistOnTarget(spelltar);
