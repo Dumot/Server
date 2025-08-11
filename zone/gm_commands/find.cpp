517cc41e2c028679c12cc4cc943b184eadf9d220
I bring dishonor to mother and father stones <201484118+Doljonijiarnimorinar@users.noreply.github.com>
2025-08-11 18:23:51 -0400

    Add GM find subcommand 'account' (#281)
    Co-authored-by: djgouac <86606378+djgouac@users.noreply.github.com>

--- zone/gm_commands/find.cpp
+++ zone/gm_commands/find.cpp
@@ -1,5 +1,6 @@
 #include "../client.h"
 #include "find/aa.cpp"
+#include "find/account.cpp"
 #include "find/body_type.cpp"
 #include "find/bot.cpp"
 #include "find/bug_category.cpp"
@@ -38,6 +39,7 @@ void command_find(Client *c, const Seperator *sep)
 
 	std::vector<Cmd> commands = {
 		Cmd{.cmd = "aa", .u = "aa [Search Criteria]", .fn = FindAA, .a = {"#findaa"}},
+		Cmd{.cmd = "account", .u = "character [Search Criteria]", .fn = FindAccount, .a = {"#findaccount"}},
 		Cmd{.cmd = "body_type", .u = "body_type [Search Criteria]", .fn = FindBodyType, .a = {"#findbodytype"}},
 		Cmd{.cmd = "bug_category", .u = "bug_category [Search Criteria]", .fn = FindBugCategory, .a = {"#findbugcategory"}},
 		Cmd{.cmd = "character", .u = "character [Search Criteria]", .fn = FindCharacter, .a = {"#findcharacter"}},
